4c6efa3cd01fe47a9874db2f39f021a84ea8bbc7
Drew Edgar <drewedgar@ANCDEdgar.local>
2017-06-09 11:21:58 -0400

    Jenkins output to xml for JUnit reporter

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -66,7 +66,7 @@ done
 	if [[ $IGNORE_UNITTEST == "false" ]]
 	then
 		echo -ne "${GREEN} Running PHPUnit Tests.....${NC}\n\n"
-		phpunit -c tests/phpunit.xml --disallow-test-output
+		phpunit -c tests/phpunit.xml --disallow-test-output --coverage-xml tests
 
 		PHPUNIT_OUT=$?
 		if [[ $PHPUNIT_OUT == 0 ]]; then
